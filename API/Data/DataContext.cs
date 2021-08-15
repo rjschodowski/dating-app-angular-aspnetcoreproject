2ddd22b8573e5aec4852d76aa9b55d05f1762f16
R.J <rjschodowski@hotmail.com>
2021-08-15 11:49:54 -0400

    DataContext.cs file updated

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -4,6 +4,11 @@ namespace API.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
     }
 }
